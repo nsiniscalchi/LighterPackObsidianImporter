7f18cf8fb73312ccdbc7d6b140b310b4b3415d76
nsiniscalchi <github@nicolasiniscalchi.com>
2025-10-19 20:40:09 +0200

    chore: update eslint to 9.38.0 and add eslint-plugin-obsidianmd; change TypeScript target to es2023

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import obsidianmd from "eslint-plugin-obsidianmd";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,15 +16,17 @@ const compat = new FlatCompat({
     allConfig: js.configs.all
 });
 
-export default defineConfig([globalIgnores(["**/node_modules/", "**/main.js"]), {
+export default defineConfig(globalIgnores(["**/node_modules/", "**/main.js"]),{
     extends: compat.extends(
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
     ),
 
     plugins: {
         "@typescript-eslint": typescriptEslint,
+        "obsidianmd": obsidianmd
     },
 
     languageOptions: {
@@ -32,8 +35,11 @@ export default defineConfig([globalIgnores(["**/node_modules/", "**/main.js"]), 
         },
 
         parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
+        parserOptions: {
+            ecmaVersion: 14,
+            sourceType: "module",
+            project: "./tsconfig.json",
+        },
     },
 
     rules: {
@@ -46,5 +52,16 @@ export default defineConfig([globalIgnores(["**/node_modules/", "**/main.js"]), 
         "@typescript-eslint/ban-ts-comment": "off",
         "no-prototype-builtins": "off",
         "@typescript-eslint/no-empty-function": "off",
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"],
+        "curly": ["error", "all"],
+        "no-eval": "error",
+        "eqeqeq": ["error", "always"],
+        "@typescript-eslint/no-base-to-string": "error",
+        "@typescript-eslint/no-floating-promises": "error",
+        "@typescript-eslint/restrict-plus-operands": ["error", { skipCompoundAssignments: false }],
+        "@typescript-eslint/restrict-template-expressions": "error",
+        "@typescript-eslint/no-unsafe-assignment": "error",
+        ...obsidianmd.configs.recommended.rules
     },
-}]);
+});
